9d0f30ae837cc04d6921a94e6e8ee6c90f00aa2d
osamaghox <126187603+osamaghox@users.noreply.github.com>
2026-02-11 00:02:42 +0300

    Update default.nix

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -1,3 +1,7 @@
+# configs to look for niri config in nixos
+# https://github.com/JotaFab/s13los/blob/main/modules/home/programs/niri/niri.nix
+# https://github.com/devluixos/luix_nix_config/tree/master/home/modules/niri
+
 
 { ... }:
 {
@@ -5,5 +9,6 @@
     ./noctalia
   ];
 }
+
 
 
