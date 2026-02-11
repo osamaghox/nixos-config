834d4ec445856bb0c360ee57e483887e48e94db6
osamaghox <126187603+osamaghox@users.noreply.github.com>
2026-02-11 06:23:27 +0300

    Update default.nix

--- modules/home/niri/noctalia/default.nix
+++ modules/home/niri/noctalia/default.nix
@@ -1,7 +1,7 @@
 # noctalia config options
 # https://docs.noctalia.dev/getting-started/nixos/#config-ref
 
-{ ... }:
+{ pkgs, inputs, ... }:
 {
   imports = [
   inputs.noctalia.homeModules.default
@@ -9,6 +9,7 @@
     ./colors.nix
   ];
 }
+
 
 
 
