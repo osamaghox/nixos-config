3daaddacd3d6a7945892df623ccd870044c9bf9d
osamaghox <126187603+osamaghox@users.noreply.github.com>
2026-02-12 00:44:19 +0300

    Update default.nix

--- modules/home/niri/noctalia/default.nix
+++ modules/home/niri/noctalia/default.nix
@@ -2,12 +2,47 @@
 # https://docs.noctalia.dev/getting-started/nixos/#config-ref
 
 { pkgs, inputs, ... }:
+
 {
-  imports = [
-    ./noctalia.nix
-    ./colors.nix
-  ];
-}
+
+  imports = [   inputs.noctalia.homeModules.default ];
+
+      programs.noctalia-shell = {
+        enable = true;
+        systemd.enable = true;
+        settings = {
+          bar = {
+            density = "compact";
+            position = "right";
+            showCapsule = false;
+            widgets = {
+              left = [
+                { id = "ControlCenter"; useDistroLogo = true; }
+                { id = "Network"; }
+                { id = "Bluetooth"; }
+              ];
+              center = [
+                { id = "Workspace"; hideUnoccupied = false; labelMode = "none"; }
+              ];
+              right = [
+                { id = "Battery"; alwaysShowPercentage = false; warningThreshold = 30; }
+                { id = "Clock"; formatHorizontal = "HH:mm"; formatVertical = "HH mm"; useMonospacedFont = true; usePrimaryColor = true; }
+              ];
+            };
+          };
+          colorSchemes.predefinedScheme = "Monochrome";
+        #  general = {
+        #    avatarImage = "/home/${username}/.face";
+        #    radiusRatio = 0.2;
+        #  };
+          location = {
+            monthBeforeDay = true;
+            name = "Marseille, France";
+          };
+        };
+      };
+    }
+
 
 
 
