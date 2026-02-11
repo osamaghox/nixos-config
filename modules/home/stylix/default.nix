5b83fa469f5db7ca536252d6216503cca0d1ef6e
osamaghox <126187603+osamaghox@users.noreply.github.com>
2026-02-11 23:37:20 +0300

    Create default.nix

---
+++ modules/home/stylix/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  stylix = {
+    enable = true;
+    image = ./wallpapers/nature.jpg; # اختر صورتك المفضلة
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    
+    # ستعرف Stylix تلقائياً أنك تستخدم Niri وتقوم بتلوين الـ focus-ring
+    targets.niri.enable = true; 
+    
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        name = "JetBrainsMono Nerd Font";
+      };
+    };
+  };
+}
