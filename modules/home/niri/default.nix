db40cb822aaee7d1d8c64fb34867792cd04ccbd9
osamaghox <126187603+osamaghox@users.noreply.github.com>
2026-02-12 04:43:14 +0300

    Update default.nix

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -3,7 +3,7 @@
 # https://github.com/devluixos/luix_nix_config/tree/master/home/modules/niri
 
 
-{ config, pkgs,username ... }:
+{ config, pkgs,username, ... }:
 {
   imports = [
     ./noctalia
@@ -13,13 +13,14 @@
 
 xdg.configFile."niri/config.kdl".source = 
     config.lib.file.mkOutOfStoreSymlink "/home/${username}/nixos-config/modules/home/niri/config.kdl";
-}
+
 
 
 
 	# xdg.configFile."swaylock/config".source = ./swaylock.conf;
   # home.file.".local/bin/nws.sh".source = ./nws.sh;
 }
+
 
 
 
