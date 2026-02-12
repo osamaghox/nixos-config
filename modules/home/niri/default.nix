ed598df2de2bc9190189f85d443b8eb3da69c343
osamaghox <126187603+osamaghox@users.noreply.github.com>
2026-02-12 03:01:00 +0300

    Update default.nix

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -11,8 +11,9 @@
     ./niri.nix
 
   ];
-	# xdg.configFile."niri/config.kdl".source = ./config.kdl;
+	xdg.configFile."niri/config.kdl".source = ./config.kdl;
 	# xdg.configFile."swaylock/config".source = ./swaylock.conf;
   # home.file.".local/bin/nws.sh".source = ./nws.sh;
 }
+
 
