c64405d75433f2bea818b77559961d5351ba6654
osamaghox <126187603+osamaghox@users.noreply.github.com>
2026-03-04 09:03:07 +0300

    te

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -1,6 +1,36 @@
-{ ... }:
+هل اقدر اسوي كذا الshell aliasses
+
+{ config, lib, pkgs, host, ... }:
+
+let
+  cfg = config.userSettings.programs.zsh;
+in
 {
-  imports = [
-    ./zsh.nix
-  ];
+  ### 1. تعريف الخيار (Option)
+  options.userSettings.programs.zsh = {
+    enable = lib.mkEnableOption "Enable Zsh with Home-Manager specific settings";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      
+      autosuggestion.enable = true;   
+      syntaxHighlighting.enable = true;
+
+      enableCompletion = true; 
+      history = {
+        size = 10000;
+        path = "${config.home.homeDirectory}/.zsh_history";
+      };
+
+      shellAliases = {
+        # نظام NixOS
+        ll = "ls -l";
+        update = "sudo nixos-rebuild switch --flake ~/nixos-config#${host}";
+        garbage = "nix-collect-garbage -d"; # تنظيف الملفات القديمة لتوفير مساحة
+        cdconf = "cd ~/nixos-config"; # انتقال سريع لمجلد الإعدادات
+      };
+    };
+  };
 }
